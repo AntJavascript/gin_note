584cbdb711145dcdac640039ea78100458b0b17b
XiaoShi <337265223@qq.com>
2023-09-08 16:42:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
-	_ "gin_note/router"
+	"example.com/m/v2/models"
+
+	_ "example.com/m/v2/router"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// 开始调试模式
 	gin.SetMode("debug")
+
+	models.ConDB()
 }
