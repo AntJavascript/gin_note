1fcac1659250741bf400bd62baaddde83cfe6534
XiaoShi <337265223@qq.com>
2023-09-08 09:27:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"fmt"
 	_ "gin_note/router"
-	"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "root:shi465608@tcp(127.0.0.1:3306)/note_app?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err == nil {
-		fmt.Println("连接成功", db)
-	} else {
-		fmt.Println(err)
-	}
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
+	// 开始调试模式
+	gin.SetMode("debug")
 }
