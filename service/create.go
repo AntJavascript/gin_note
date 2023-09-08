40adf6b63f24b37bdb325e8c659ac025e8ffade6
XiaoShi <337265223@qq.com>
2023-09-08 14:59:02 +0800

    Update create.go

--- service/create.go
+++ service/create.go
@@ -1,1 +1,25 @@
 package service
+import (
+	"example.com/m/v2/models"
+)
+
+var Record = new(recordService)
+
+type recordService struct{}
+
+func(c *recordService) GetList(req *models.Record) ([]models.Record, int, error) {
+	// 数据处理
+	var result []models.Record
+	// 总数
+	var count int = 10
+	for i := 0; i <= count; i++ {
+		item := models.Record{
+			Id: i,
+			Type: "income",
+		}
+		// 加入数组
+		result = append(result, item)
+	}
+
+	return result, count, nil
+}
