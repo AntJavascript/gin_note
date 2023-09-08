015278d63a6ed60637b119878769e39fc9a65cb0
XiaoShi <337265223@qq.com>
2023-09-08 10:18:16 +0800

    Create record.go

---
+++ models/record.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+)
+
+// 收支记录字段
+type Record struct {
+	Id         int      // id
+	Type       string // income(收入)、expend(支出)
+	RecordDate string    // 日期
+	Amount     float64   // 金额
+	RecordType string    // 收支类型（用于展示图标）
+	Remark     string    // 备注
+	Account    string    // 账户信息（手机号码）
+	Created    time.Time  // 创建时间
+}
