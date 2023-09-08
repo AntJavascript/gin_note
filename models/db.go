7cb0d4326e2232987415f83da42acf3c1b3f4be0
XiaoShi <337265223@qq.com>
2023-09-08 16:42:28 +0800

    Update db.go

--- models/db.go
+++ models/db.go
@@ -1,1 +1,19 @@
 package models
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConDB() {
+	dsn := "root:shi465608@tcp(127.0.0.1:3306)/note_app?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("数据库连接失败", err)
+	} else {
+		fmt.Println("数据库连接成功", DB)
+	}
+}
