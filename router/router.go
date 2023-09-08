e819f6310b5a430148b57bba0ff2a55fe92af2a3
XiaoShi <337265223@qq.com>
2023-09-08 09:41:38 +0800

    Update router.go

--- router/router.go
+++ router/router.go
@@ -7,14 +7,14 @@ import (
 )
 
 func init() {
-  // 初始化
+  	// 初始化
 	router := gin.Default()
-  // 登录验证中间件
+  	// 登录验证中间件
 	router.Use(middleware.CheckLogin())
-  // 设置静态资源路由
+  	// 设置静态资源路由
 	router.Static("/static", "../static/img")
 
-  /* 收支记录 */
+  	/* 收支记录 */
 	user := router.Group("record")
 	{
 		user.GET("/list", controllers.Record.List)
@@ -24,6 +24,6 @@ func init() {
 		user.POST("/delete/:id", controllers.Record.Delete)
 	}
   
-  // 启动
+  	// 启动
 	router.Run(":9099")
 }
