a9cff284fc423be7a6fde5c542b7139f2c6be6a8
XiaoShi <337265223@qq.com>
2023-09-08 09:40:46 +0800

    Update router.go

--- router/router.go
+++ router/router.go
@@ -1,1 +1,29 @@
 package router
+
+import (
+	"gin_note/controller"
+	"gin_note/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+  // 初始化
+	router := gin.Default()
+  // 登录验证中间件
+	router.Use(middleware.CheckLogin())
+  // 设置静态资源路由
+	router.Static("/static", "../static/img")
+
+  /* 收支记录 */
+	user := router.Group("record")
+	{
+		user.GET("/list", controller.Record.List)
+		user.GET("/detail/:id", controller.Record.Detail)
+		user.POST("/add", controller.Record.Add)
+		user.POST("/update", controller.Record.Update)
+		user.POST("/delete/:id", controller.Record.Delete)
+	}
+  
+  // 启动
+	router.Run(":9099")
+}
