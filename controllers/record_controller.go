2689fd73dbc04ad6beb55c29bd02bc372ff98847
XiaoShi <337265223@qq.com>
2023-09-08 14:56:57 +0800

    Update record_controller.go

--- controllers/record_controller.go
+++ controllers/record_controller.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
-	"gin_note/models"
+	"example.com/m/v2/service"
+	"example.com/m/v2/models"
+	"example.com/m/v2/tools"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 var Record = new(recordCtrl)
 
@@ -11,29 +14,16 @@ type recordCtrl struct{}
 func (c *recordCtrl) List(ctx *gin.Context) {
 	// 参数
 	var req *models.Record
-	if err := ctx.ShouldBind(&req); err != nil {
-		ctx.JSON(http.StatusOK, common.JsonResult{
-			Code: -1,
-			Msg:  err.Error(),
-		})
-		return
-	}
 
 	// 调用获取列表方法
 	list, count, err := service.Record.GetList(req)
 	if err != nil {
-		ctx.JSON(http.StatusOK, common.JsonResult{
-			Code: -1,
-			Msg:  err.Error(),
-		})
+		ctx.JSON(http.StatusOK, tools.JsonReturn("","查询失败", 400))
 		return
 	}
+	res := tools.JsonReturn(list, "查询成功", 200)
+	res["count"] = count
 
 	// 返回结果
-	ctx.JSON(http.StatusOK, common.JsonResult{
-		Code:  0,
-		Data:  list,
-		Msg:   "查询成功",
-		Count: count,
-	})
+	ctx.JSON(http.StatusOK, res)
 }
