4fc1026f03061a534274bd0b884b8d26aa23b18b
XiaoShi <337265223@qq.com>
2023-09-08 17:38:44 +0800

    Update record_controller.go

--- controllers/record_controller.go
+++ controllers/record_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"strconv"
 	"example.com/m/v2/service"
 	"example.com/m/v2/dto"
 	"example.com/m/v2/tools"
@@ -12,20 +13,41 @@ var Record = new(recordCtrl)
 type recordCtrl struct{}
 
 func (c *recordCtrl) List(ctx *gin.Context) {
-	// 参数
+
+	res := make(map[string]interface{})
+
+	defer ctx.JSON(http.StatusOK, res)
+
 	var req *dto.Record
 
 	// 调用获取列表方法
 	list, count, err := service.Record.GetList(req)
 	if err != nil {
-		ctx.JSON(http.StatusOK, tools.JsonReturn("","查询失败", 400))
 		return
 	}
-	res := tools.JsonReturn(list, "查询成功", 200)
+	res = tools.JsonReturn(list, "查询成功", 200)
 	token, err := tools.GenerateToken("15817351609", "shi@465608")
 	res["count"] = count
 	res["token"] = token
 
-	// 返回结果
-	ctx.JSON(http.StatusOK, res)
+}
+
+func (c *recordCtrl) Detail(ctx *gin.Context) {
+
+	postId := ctx.Param("id")
+	id, _ := strconv.Atoi(postId)
+	var req *dto.Record
+	
+	res := make(map[string]interface{})
+
+	defer ctx.JSON(http.StatusOK, res)
+
+	if postId != "" {
+		detail, err := service.Record.Detail(req, id)
+		if err != nil {
+			res = tools.JsonReturn("", "查询失败", 400)
+		} else {
+			res = tools.JsonReturn(detail, "查询成功", 200)
+		}
+	}
 }
