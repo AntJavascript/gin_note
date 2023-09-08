e42964d621cd7f7baca86a0eecbe167b406ac02d
XiaoShi <337265223@qq.com>
2023-09-08 10:19:34 +0800

    Update record_controller.go

--- controllers/record_controller.go
+++ controllers/record_controller.go
@@ -1,22 +1,11 @@
 package controllers
 
 import (
+	"gin_note/models"
 	"github.com/gin-gonic/gin"
 )
 
-// 收支记录字段
-type Record struct {
-	Id         int      // id
-	Type       string // income(收入)、expend(支出)
-	RecordDate string    // 日期
-	Amount     float64   // 金额
-	RecordType string    // 收支类型（用于展示图标）
-	Remark     string    // 备注
-	Account    string    // 账户信息（手机号码）
-	Created    time.Time  // 创建时间
-}
-
-func (c *Record) List(ctx *gin.Context) {
+func (c *models.Record) List(ctx *gin.Context) {
 	// 参数
 	var req *dto.UserPageReq
 	if err := ctx.ShouldBind(&req); err != nil {
