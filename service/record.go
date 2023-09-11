35ae05c72d760783ff046f9a5fc90fffd302af69
XiaoShi <337265223@qq.com>
2023-09-11 14:56:11 +0800

    Update record.go

--- service/record.go
+++ service/record.go
@@ -11,21 +11,11 @@ var Record = new(recordService)
 
 type recordService struct{}
 
-func (c *recordService) GetList(req *dto.Record) ([]dto.Record, int, error) {
+func (c *recordService) GetList(req *dto.Record, date string) ([]dto.Record, int, error) {
 	// 数据处理
 	var result []dto.Record
-	// 总数
-	var count int = 10
-	for i := 0; i <= count; i++ {
-		item := dto.Record{
-			Id:   i,
-			Type: "income",
-		}
-		// 加入数组
-		result = append(result, item)
-	}
-
-	return result, count, nil
+	models.DB.Where("created = ?", date).Find(&result)
+	return result, len(result), nil
 }
 
 func (c *recordService) Detail(req *dto.Record, id int) (dto.Record, error) {
