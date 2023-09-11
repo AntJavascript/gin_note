7418979b45eaa8ae8eaa6a3e3aca812f64f2f152
XiaoShi <337265223@qq.com>
2023-09-11 15:05:43 +0800

    Update record_controller.go

--- controllers/record_controller.go
+++ controllers/record_controller.go
@@ -17,7 +17,6 @@ type recordCtrl struct{}
 
 // 根据日期查询列表
 func (c *recordCtrl) List(ctx *gin.Context) {
-
 	res := make(map[string]interface{})
 	
 	date := ctx.Query("date") // 请求日期
@@ -29,30 +28,30 @@ func (c *recordCtrl) List(ctx *gin.Context) {
 		date = fmt.Sprintf("%s-%s-%s", year, month, day)
 	}
 	fmt.Println(date)
-	var req *dto.Record
 
 	// 调用获取列表方法
+	var req *dto.Record
 	list, count, err := service.Record.GetList(req, date)
 	if err != nil {
-		return
+		res = tools.JsonReturn(list, "查询失败", 400)
+	} else {
+		res = tools.JsonReturn(list, "查询成功", 200)
+		token, err := tools.GenerateToken("15817351609", "shi@465608")
+		res["count"] = count
+		res["token"] = token
 	}
-	res = tools.JsonReturn(list, "查询成功", 200)
-	token, err := tools.GenerateToken("15817351609", "shi@465608")
-	res["count"] = count
-	res["token"] = token
+	
 	ctx.JSON(http.StatusOK, res)
 }
 
 // 查询单条记录详情
 func (c *recordCtrl) Detail(ctx *gin.Context) {
-
 	postId := ctx.Param("id")
 	id, _ := strconv.Atoi(postId)
-	var req *dto.Record
-	fmt.Println(postId, id)
 	res := make(map[string]interface{})
 
 	if postId != "" {
+		var req *dto.Record
 		detail, err := service.Record.Detail(req, id)
 		if err != nil {
 			res = tools.JsonReturn("", "查询失败", 400)
