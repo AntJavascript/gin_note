07a0ae544976b1a35e29aa7f06d85a7ebe3257ce
XiaoShi <337265223@qq.com>
2023-09-12 10:20:33 +0800

    Update record.go

--- dto/record.go
+++ dto/record.go
@@ -1,9 +1,5 @@
 package dto
 
-import (
-	"time"
-)
-
 // 收支记录字段
 type Record struct {
 	Id         int      `json:"id"`      // id
@@ -13,5 +9,4 @@ type Record struct {
 	RecordType string   `json:"record_type"`    // 收支类型（用于展示图标）
 	Remark     string   `json:"remark"`    // 备注
 	Account    string   `json:"account"`    // 账户信息（手机号码）
-	Created    time.Time  `json:"created"`  // 创建时间
 }
