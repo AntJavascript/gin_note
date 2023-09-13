b042b7831e6f8cdda8bc9d028e9dc5a7eae8a2b1
XiaoShi <337265223@qq.com>
2023-09-13 11:23:37 +0800

    Update user.go

--- service/user.go
+++ service/user.go
@@ -11,7 +11,7 @@ var User = new(userService)
 type userService struct{}
 
 func (c *userService) Login(params *dto.User) (string, error) {
-	token, err := tools.GenerateToken(params.Phone, params.Password)
+	token, err := tools.GenerateToken(params.Phone, params.Password, 2)
 	return token, err
 }
 
