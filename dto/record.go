402a684e7e4cb79468968793e841a44e16e7f90c
XiaoShi <337265223@qq.com>
2023-09-16 08:41:50 +0800

    Update record.go

--- dto/record.go
+++ dto/record.go
@@ -1,17 +1,13 @@
 package dto
 
-import (
-	"time"
-)
-
 // 收支记录字段
 type Record struct {
 	Id         int      `json:"id"`      // id
 	Type       string   `json:"type"` // income(收入)、expend(支出)
 	RecordDate string   `json:"record_date"`    // 日期
+	RecordDateUnix int64 `json:"record_date_unix"`    // 时间戳
 	Amount     float64  `json:"amount"`   // 金额
 	RecordType string   `json:"record_type"`    // 收支类型（用于展示图标）
 	Remark     string   `json:"remark"`    // 备注
 	Account    string   `json:"account"`    // 账户信息（手机号码）
-	Created    time.Time  `json:"created"`  // 创建时间
 }
