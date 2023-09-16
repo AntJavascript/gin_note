3f45d25e9e891798d8d65e7c61ded37c2aef718b
XiaoShi <337265223@qq.com>
2023-09-16 08:42:52 +0800

    Update user.go

--- models/user.go
+++ models/user.go
@@ -1,16 +1,15 @@
 package models
 
 import (
-	"time"
+	"gorm.io/gorm"
 )
 
 // 用户表字段
 type User struct {
-	Id       int
+	gorm.Model
 	UserName string
 	Password string
 	Email    string
-	Created  time.Time `gorm:"autoUpdateTime:false"` // 注册时间
 	Phone    string
 	Face     string // 用户头像
 	Status   int    // 账号状态
