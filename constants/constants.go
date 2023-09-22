eee5f1efa5f089fff233c22d69e6461871a29f14
XiaoShi <337265223@qq.com>
2023-09-22 14:48:56 +0800

    Update constants.go

--- constants/constants.go
+++ constants/constants.go
@@ -6,13 +6,13 @@ import (
 
 const (
 	// token日期
-  ACCESSTOKEN  = 2 // 2小时
-  REFRESHTOKEN = 720 // 30天
+	ACCESSTOKEN  = 2 // 2小时
+	REFRESHTOKEN = 720 // 30天
 
 	// 返回msg提示语
 	SUCCESS = "success"
 	FAIL    = "fail"
-
+	
 	// 返回状态码
 	STATUSOK = http.StatusOK
 )
