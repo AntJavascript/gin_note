9a73826cc9fddf303fec1cf7741777fb6eb11d0d
XiaoShi <337265223@qq.com>
2023-09-25 16:37:48 +0800

    Update user.go

--- models/user.go
+++ models/user.go
@@ -7,9 +7,9 @@ import (
 // 用户表字段
 type User struct {
 	gorm.Model
-	UserName string
+	UserName string `gorm:"varchar(20);not null"`
 	Password string `gorm:"size:255;not null"`
-	Email    string
+	Email    string `gorm:"varchar(100);not null"`
 	Phone    string  `gorm:"varchar(11);not null;unique"`
 	Face     string // 用户头像
 	Status   int    // 账号状态
