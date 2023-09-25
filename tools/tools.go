121650d9ccc0c487a29c7edbd503e39c18cc0d36
XiaoShi <337265223@qq.com>
2023-09-25 16:59:34 +0800

    Update tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -15,9 +15,10 @@ func JsonReturn(ctx *gin.Context, data interface{}, msg interface{}, code int) m
 
 	// 获取新的token
 	access_token, refresh_token := GetNewToken(ctx)
-	res["access_token"] = access_token
-	res["refresh_token"] = refresh_token
-
+	if (access_token != "" && refresh_token != "") {
+		res["access_token"] = access_token
+		res["refresh_token"] = refresh_token
+	}
 	return res
 }
 
