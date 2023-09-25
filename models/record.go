23de304fea4bfbb6c09ffd1c0539534f526db325
XiaoShi <337265223@qq.com>
2023-09-25 16:38:46 +0800

    Update record.go

--- models/record.go
+++ models/record.go
@@ -4,14 +4,14 @@ import (
 	"gorm.io/gorm"
 )
 
-// 收支记录字段
+// 收支记录字段 gorm: 数据库相关、json:前端传参相关
 type Record struct {
 	gorm.Model
 	Type       string  `gorm:"default:'expend';not null"`    // income(收入)、expend(支出)
-	RecordDate string  `gorm:"not null"`    // 日期
-	RecordDateUnix int64  `gorm:"not null"`    // 时间戳
+	RecordDate string  `gorm:"not null" json:"record_date"`    // 日期
+	RecordDateUnix int64  `gorm:"not null" json:"record_date_unix"`    // 时间戳
 	Amount     float64  `gorm:"not null"`   // 金额
-	RecordType string  `gorm:"not null"`    // 收支类型（用于展示图标）
+	RecordType string  `gorm:"not null" json:"record_type"`    // 收支类型（用于展示图标）
 	Remark     string    // 备注
 	Account    string  `gorm:"type:varchar(11);not null"`    // 账户信息（手机号码）
 }
